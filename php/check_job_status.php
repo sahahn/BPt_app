832aa232ba4a0f2841ae197aa7ce1517741e1fad
sahahn <sahahn@uvm.edu>
2020-07-22 16:17:09 -0400

    On return job status also return logs

--- php/check_job_status.php
+++ php/check_job_status.php
@@ -13,32 +13,39 @@ $job_dr = $project_jobs_dr."/".$_GET['job_name'];
 $error_file = $job_dr."/Error_Output.json";
 $progress_file = $job_dr."/progress.txt";
 $results_file = $job_dr."/results.pkl";
+$logs_file = $job_dr."/ABCD_ML_Logs".$_GET['job_name']."/logs.txt";
+
+// Make return array
+$return = array();
+
+// Init with unknown error, and no logs replace if not the case.
+$return['status'] = "unknown error";
+$return['logs'] = 'No saved logs found';
 
 // Make sure the job dr itself exists
 if (is_dir($job_dr)) {
 
-    // If an error return the contents then delete the job_dr
+    // If an error return the contents (dont delete the job directory)
     if (file_exists($error_file)) {
-        echo file_get_contents($error_file);
+        $return['status'] = file_get_contents($error_file);
     }
 
-    // If results, return done
+    // If done set to done
     else if (file_exists($results_file)) {
-        echo json_encode('done');
+        $return['status'] = 'done';
     }
 
-    // Return the contents of progress if not yet done
+    // If in progress, return progress
     else if (file_exists($progress_file)) {
-        echo json_encode(file_get_contents($progress_file));
+        $return['status'] = file_get_contents($progress_file);
     }
 
-    else {
-        echo json_encode("unknown error");
+    // Check for logs file
+    if (file_exists($logs_file)) {
+        $return['logs'] = file_get_contents($logs_file);
     }
-
 } 
-else {
-    echo json_encode("unknown error");
-}
 
+// Encode and return
+echo json_encode($return);
 ?>
