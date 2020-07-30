f1610d00866c61c64c9697f0ac75fbe1ccf8fbd2
sahahn <sahahn@uvm.edu>
2020-07-30 15:47:27 -0400

    Add print

--- WebApp/python/setup_info.py
+++ WebApp/python/setup_info.py
@@ -454,3 +454,4 @@ if __name__ == "__main__":
     shutil.chown(cache_dr, user='www-data', group='www-data')
     save_ML_options(os.path.join(cache_dr, 'ML_options.json'))
     save_default_params(os.path.join(cache_dr, 'default_param_dists.json'))
+    print('done')
