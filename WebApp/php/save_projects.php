10c5a7923bcd4435b630ddba0e237a1fe708919c
sahahn <sahahn@uvm.edu>
2020-07-30 15:04:08 -0400

    Debug

--- WebApp/php/save_projects.php
+++ WebApp/php/save_projects.php
@@ -1,12 +1,14 @@
 <?php
 
 include '/var/www/html/data/config.php';
-echo $user_dr;
 
 // Make user directory if it doesn't exist
 if(!is_dir($user_dr)){
     mkdir($user_dr, 0777, true);
+    echo '1';
 }
+
+echo '2';
 
 // Save the passed projects
 file_put_contents($user_dr.'/Projects.json', json_encode($_POST));
