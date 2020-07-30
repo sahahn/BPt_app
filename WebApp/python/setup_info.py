d0ba0a789bc89da0d64f1e74cb291ae24d19f331
sahahn <sahahn@uvm.edu>
2020-07-30 15:46:49 -0400

    Update

--- WebApp/python/setup_info.py
+++ WebApp/python/setup_info.py
@@ -451,6 +451,6 @@ if __name__ == "__main__":
 
     cache_dr = '/var/www/html/data/bpt/'
     os.makedirs(cache_dr, exist_ok=True)
-    shutil.chown(cache_dr, user='www-data')
+    shutil.chown(cache_dr, user='www-data', group='www-data')
     save_ML_options(os.path.join(cache_dr, 'ML_options.json'))
     save_default_params(os.path.join(cache_dr, 'default_param_dists.json'))
