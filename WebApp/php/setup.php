21e105ecc8222af94bd3666bd6ad8baf6f4293b7
sahahn <sahahn@uvm.edu>
2020-07-31 14:43:47 -0400

    indl

--- WebApp/php/setup.php
+++ WebApp/php/setup.php
@@ -1,5 +1,5 @@
 <?php
-
+include '/var/www/html/data/config.php';
 $cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
 $cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'/setup.py"';
 $cmd = $cmd_p1.$cmd_p2;
