04584c3b34af48d005ea8a346dd2b945de3943dd
sahahn <sahahn@uvm.edu>
2020-07-31 14:42:32 -0400

    dfg

--- WebApp/php/setup.php
+++ WebApp/php/setup.php
@@ -3,5 +3,6 @@
 $cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
 $cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'/setup.py"';
 $cmd = $cmd_p1.$cmd_p2;
+echo($cmd);
 exec($cmd);
 ?>
