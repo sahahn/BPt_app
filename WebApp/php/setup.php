37de023920f620e847f8ef99ec334f3113a2265c
sahahn <sahahn@uvm.edu>
2020-07-31 14:47:22 -0400

    forgot info

--- WebApp/php/setup.php
+++ WebApp/php/setup.php
@@ -1,7 +1,7 @@
 <?php
 include '/var/www/html/data/config.php';
 $cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
-$cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup.py"';
+$cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup_info.py"';
 $cmd = $cmd_p1.$cmd_p2;
 echo($cmd);
 exec($cmd);
