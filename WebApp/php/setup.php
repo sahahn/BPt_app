9e966f5d84bec7e5f0d622af23d0d0ebb236a941
sahahn <sahahn@uvm.edu>
2020-08-03 13:51:15 -0400

    add try to start db

--- WebApp/php/setup.php
+++ WebApp/php/setup.php
@@ -1,5 +1,10 @@
 <?php
 include '/var/www/html/data/config.php';
+
+// Try to start database
+exec("monetdbd start /var/www/html/db");
+exec("monetdb start abcd");
+
 $cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
 $cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup_info.py"';
 $cmd = $cmd_p1.$cmd_p2;
