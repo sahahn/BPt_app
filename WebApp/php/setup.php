c2656051713e68bd2161390e574457b4eca60f81
sahahn <sahahn@uvm.edu>
2020-08-03 13:59:54 -0400

    twest

--- WebApp/php/setup.php
+++ WebApp/php/setup.php
@@ -2,10 +2,9 @@
 include '/var/www/html/data/config.php';
 
 // Try to start database
-exec("/bin/bash -c \"monetdbd start /var/www/html/db\"");
-echo exec("/bin/bash -c \"monetdb start abcd\"");
+echo exec("monetdbd start /var/www/html/db");
+echo exec("monetdb start abcd");
 
-echo "run";
 
 $cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
 $cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup_info.py"';
