f666d6e6ff1670e814f12196189e02bdb7ec4706
sahahn <sahahn@uvm.edu>
2020-08-04 11:44:37 -0400

    try running setup db in background

--- WebApp/php/setup.php
+++ WebApp/php/setup.php
@@ -6,7 +6,8 @@ $cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup_info.py"';
 $cmd = $cmd_p1.$cmd_p2;
 exec($cmd);
 
-$cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup_db.py"';
+$cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
+$cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc."setup_db.py > /dev/null &\"";
 $cmd = $cmd_p1.$cmd_p2;
 exec($cmd);
 ?>
