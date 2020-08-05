ffec6f09c1d2447f87b256d149691083ee62fe38
sahahn <sahahn@uvm.edu>
2020-08-05 11:23:21 -0400

    rel path

--- WebApp/Sets/index.php
+++ WebApp/Sets/index.php
@@ -8,13 +8,13 @@
     <meta name="author" content="">
 
     <!-- Bootstrap -->
-    <link href="WebApp/css/bootstrap.min.css" rel="stylesheet">
-    <link href="WebApp/css/bootstrap-toggle.min.css" rel="stylesheet">
-    <link href="WebApp/css/bootstrap-editable.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap-toggle.min.css" rel="stylesheet">
+    <link href="../css/bootstrap-editable.css" rel="stylesheet">
 
     <!-- Select2 -->
-    <link href="WebApp/css/select2-bootstrap4.min.css" rel="stylesheet" type="text/css">
-    <link href="WebApp/css/select2.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/select2-bootstrap4.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/select2.min.css" rel="stylesheet" type="text/css">
   </head>
   
   <body spellcheck="false">
@@ -22,10 +22,10 @@
      hi
     </div>
 
-  <script type="text/javascript" src="WebApp/js/libraries/jquery-3.2.1.min.js"></script>
-  <script type="text/javascript" src="WebApp/js/libraries/popper.min.js"></script>
-  <script type="text/javascript" src="WebApp/js/libraries/bootstrap.min.js"></script>
-  <script type="text/javascript" src="WebApp/js/libraries/select2.min.js"></script>
+  <script type="text/javascript" src="../js/libraries/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="../js/libraries/popper.min.js"></script>
+  <script type="text/javascript" src="../js/libraries/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../js/libraries/select2.min.js"></script>
   <script type="text/javascript" src="js/customSets.js"></script>
 
   </body>
