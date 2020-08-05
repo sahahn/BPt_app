c83362037adb336ce0ff4ac2047ab94c37513e62
sahahn <sahahn@uvm.edu>
2020-08-05 10:50:13 -0400

    Forgot old debug statements were still there...

--- WebApp/python/load_target.py
+++ WebApp/python/load_target.py
@@ -1,29 +1,13 @@
 import argparse
 from loading import variable_load
-import pymonetdb
-
-
 
 
 def main(user_dr, n):
-
-    try:
-        conn = pymonetdb.connect(username="monetdb", password="monetdb",
-                            hostname="localhost", port=11223, database="abcd")
-        with open('/tmp/hi', 'w') as f:
-            f.write('worked')
-    except:
-        with open('/tmp/hi', 'w') as f:
-            f.write('failed')
-
 
     variable_load(user_dr, 'target', n)
 
 
 if __name__ == "__main__":
-
-    with open('/tmp/hi', 'w') as f:
-        f.write('!')
 
     parser = argparse.ArgumentParser(description='ABCD_ML Load Target Script')
     parser.add_argument('user_dr', type=str,
