051a2d982b17827283cc588bcdfc77f897218d57
sahahn <sahahn@uvm.edu>
2020-08-05 11:18:59 -0400

    move sets within web app

---
+++ WebApp/Sets/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Sets of Variables</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Bootstrap -->
+    <link href="WebApp/css/bootstrap.min.css" rel="stylesheet">
+    <link href="WebApp/css/bootstrap-toggle.min.css" rel="stylesheet">
+    <link href="WebApp/css/bootstrap-editable.css" rel="stylesheet">
+
+    <!-- Select2 -->
+    <link href="WebApp/css/select2-bootstrap4.min.css" rel="stylesheet" type="text/css">
+    <link href="WebApp/css/select2.min.css" rel="stylesheet" type="text/css">
+  </head>
+  
+  <body spellcheck="false">
+    <div class="container-fluid" style="margin-top: 10px;">
+     hi
+    </div>
+
+  <script type="text/javascript" src="WebApp/js/libraries/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="WebApp/js/libraries/popper.min.js"></script>
+  <script type="text/javascript" src="WebApp/js/libraries/bootstrap.min.js"></script>
+  <script type="text/javascript" src="WebApp/js/libraries/select2.min.js"></script>
+  <script type="text/javascript" src="js/customSets.js"></script>
+
+  </body>
+</html>
