59cf2c2ae3080ceeab28b8e583dab169be4eca39
sahahn <sahahn@uvm.edu>
2020-08-05 15:16:06 -0400

    try

--- WebApp/js/code/sets.js
+++ WebApp/js/code/sets.js
@@ -17,12 +17,12 @@ function initSetTable(set_id, variables) {
     var table = jQuery('#table-'+set_id);
     var rm_btn = '<button class="btn btn-sm btn-danger set-rm">Remove</button>';
     table.DataTable({
-        dom: '<"top"li>rt<"bottom"p>',
+        dom: '<"top"li>rt<"bottom"pf>',
         data: variables.map(v => [v, rm_btn]),
         columns: [{title: "Variable"},  {title: "", orderable: false, className: "text-center"}],
         scrollX: true,
         autoWidth: true,
-        searching: false,
+        searching: true,
         lengthChange: true,
         lengthMenu: [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
     });
