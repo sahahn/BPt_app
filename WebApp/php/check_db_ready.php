99f15e7f4ac69181be0da96c5800684a4fafface
sahahn <sahahn@uvm.edu>
2020-08-06 12:45:47 -0400

    this?

--- WebApp/php/check_db_ready.php
+++ WebApp/php/check_db_ready.php
@@ -7,8 +7,8 @@ $all_events_loc = $data_dr.'all_events.json';
 if (file_exists($loaded_loc)) {
 
     $return = array();
-    $return['loaded'] = file_get_contents($loaded_loc);
-    $return['all_events'] = file_get_contents($all_events_loc);
+    $return['loaded'] = json_encode(file_get_contents($loaded_loc));
+    $return['all_events'] = json_encode(file_get_contents($all_events_loc));
     echo json_encode($return);
 }
 else {
