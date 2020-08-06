d5f06f79d53242215be0b286a6c14ade0b084d32
sahahn <sahahn@uvm.edu>
2020-08-06 12:46:50 -0400

    like thjis

--- WebApp/php/check_db_ready.php
+++ WebApp/php/check_db_ready.php
@@ -7,9 +7,9 @@ $all_events_loc = $data_dr.'all_events.json';
 if (file_exists($loaded_loc)) {
 
     $return = array();
-    $return['loaded'] = json_encode(file_get_contents($loaded_loc));
-    $return['all_events'] = json_encode(file_get_contents($all_events_loc));
-    echo json_encode($return);
+    $return['loaded'] = file_get_contents($loaded_loc);
+    $return['all_events'] = file_get_contents($all_events_loc);
+    echo $return;
 }
 else {
     echo json_encode('not ready');
