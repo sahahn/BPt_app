2190aa201f0f4cf60024fed942cccb18d82a8d80
sahahn <sahahn@uvm.edu>
2020-08-06 12:22:15 -0400

    add lock and save erros to writing db

--- WebApp/python/setup_db.py
+++ WebApp/python/setup_db.py
@@ -131,32 +131,50 @@ def upload_custom_data(custom_dr, con, all_events):
     
 def main():
 
-    # Make connection
-    db_dr = '/var/www/html/data/bpt/db'
-    con = sqlite3.connect(db_dr)
-
-    # Load or init all events
-    all_events_loc = '/var/www/html/data/bpt/all_events.json'
-    if (os.path.isfile(all_events_loc)):
-        with open(all_events_loc, 'r') as f:
-            all_events = set(json.load(f))
+    # Check for db-add lock
+    lock_loc = '/var/www/html/data/bpt/lock'
+    if (os.path.isfile(lock_loc)):
+        return None
     else:
-        all_events = set()
-    
-    # Try to upload all custom data
-    custom_dr = '/var/www/html/data/sources/custom/'
-    all_events = upload_custom_data(custom_dr, con, all_events)
+        with open(lock_loc, 'w') as f:
+            f.write('locked')
 
-    # When done with uploads, save a json with the
-    # current loaded tables
-    loaded_loc = '/var/www/html/data/bpt/loaded.json'
-    loaded = get_all_tables(con)
-    with open(loaded_loc, 'w') as f:
-        json.dump(loaded, f)
+    try:
 
-    # Save all events
-    with open(all_events_loc, 'w') as f:
-        json.dump(sorted(list(all_events)), f)
+        # Make connection
+        db_dr = '/var/www/html/data/bpt/db'
+        con = sqlite3.connect(db_dr)
+
+        # Load or init all events
+        all_events_loc = '/var/www/html/data/bpt/all_events.json'
+        if (os.path.isfile(all_events_loc)):
+            with open(all_events_loc, 'r') as f:
+                all_events = set(json.load(f))
+        else:
+            all_events = set()
+        
+        # Try to upload all custom data
+        custom_dr = '/var/www/html/data/sources/custom/'
+        all_events = upload_custom_data(custom_dr, con, all_events)
+
+        # When done with uploads, save a json with the
+        # current loaded tables
+        loaded_loc = '/var/www/html/data/bpt/loaded.json'
+        loaded = get_all_tables(con)
+        with open(loaded_loc, 'w') as f:
+            json.dump(loaded, f)
+
+        # Save all events
+        with open(all_events_loc, 'w') as f:
+            json.dump(sorted(list(all_events)), f)
+
+    # If any error
+    except Exception as e:
+        with open('/var/www/html/data/bpt/add_to_db_errors.txt', 'w') as f:
+            f.write(repr(e))
+
+    # Remove the lock
+    os.remove(lock_loc)
 
 
 if __name__ == "__main__":
