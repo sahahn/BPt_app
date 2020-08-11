a763d5aa15bef56d3d77021339d5c4ae3f4b1da0
sahahn <sahahn@uvm.edu>
2020-08-11 14:46:04 -0400

    same

--- WebApp/js/code/test.js
+++ WebApp/js/code/test.js
@@ -212,10 +212,15 @@ function setTestResults(output, key, project) {
     table_html = table_html + '</tbody></table>';
     jQuery('#'+key+'-table').append(table_html);
 
-    console.log(jQuery('#' + key + '-table').width());
+    console.log(jQuery('#' + key + '-table').width()); 
+
+    var to_scroll_X = false;
+    if (jQuery('#' + key + '-table').width() > 800) {
+        to_scroll_X = true;
+    }
     
     $('#'+key+'-real-table').DataTable({
-        "scrollY": 500,
+        "scrollX": to_scroll_X,
         "searching": true,
         "paging": true,
         "info": true,
