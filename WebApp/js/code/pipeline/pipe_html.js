d709da21571a85dcf26b415dbce9880fae40262f
sahahn <sahahn@uvm.edu>
2020-08-14 11:43:31 -0400

    Update pipeline documentation

--- WebApp/js/code/pipeline/pipe_html.js
+++ WebApp/js/code/pipeline/pipe_html.js
@@ -55,7 +55,16 @@ function getObjHTML(key, getBaseHTML, bold_text) {
 
 function getBaseModelHTML(key, prepend='') {
     
-    var model_descr = 'Selection of which ML Model to use';
+    var model_descr = 'The choice of model. This is explicitly the pipeline piece ' +
+    'responsible for making predictions given the input passed to it from the earlier ML Pipeline steps. ' +
+    'The model can either be a single estimator, or can be an Ensemble, which represents different strategies ' +
+    'for combining the predictions from multiple base learners. Note: In some cases the distinction between Models and Ensembles ' +
+    'is not always super clear. For example, Random Forests are an ensemble based model, but are not listed under Ensembles within the ' +
+    'choices here. The reason for that is that that the choice of base ensemble model is fixed as a decision tree, vs. other ensemble methods ' +
+    'where the choice of a base learner is more flexible. In this sense, we consider Ensemble methods as those which require the selection of one ' +
+    'or more base models, rather then its more literal and explicit meaning.<br><br>' +
+    'Note: Models are sensitive to the type of the ML Pipeline, for example there may be different avaliable options ' +
+    'for regression based ML Pipelines vs. binary ones.';
     var model_label = getPopLabel(key, prepend + "Model ", model_descr, '-obj-input');
 
     var html = getBaseObjHTML(key, model_label);
@@ -100,7 +109,12 @@ function getBaseEnsembleModelHTML(key) {
 
 function getBaseFeatSelectorHTML(key) {
 
-    var obj_descr = 'Selection of Feature Selector to use';
+    var obj_descr = 'The, optional, selection of which feature selector to use. ' +
+    'Within BPt feature selectors are any pipeline objects which perform some sort of explicit feature selection ' +
+    '(conceptually different then dimensionality reduction techniques like PCA). For example, any piece ' +
+    'that takes in some set of features and essentially passes on a reduced subset of those original features.<br><br>' +
+    'Note: Feature Selectors are sensitive to the type of the ML Pipeline, for example there may be different avaliable options ' +
+    'for regression based ML Pipelines vs. binary ones.';
     var obj_label = getPopLabel(key, "Feature Selector ", obj_descr, '-obj-input');
 
     var html = getBaseObjHTML(key, obj_label);
@@ -114,7 +128,15 @@ function getBaseFeatSelectorHTML(key) {
 
 function getBaseImputerHTML(key) {
 
-    var obj_descr = 'Selection of Imputer to use';
+    var obj_descr = 'Selection of which Imputer / imputation strategy to use. ' +
+    'If there is any missing data (NaN’s) that have been kept within the loaded data, ' +
+    'then an imputation strategy must be defined! ' +
+    'This pipeline piece allows you to define an imputation strategy. ' +
+    'One important consideration to take into account when choosing and defining Imputers is ' +
+    'the associated scope of that Imputer. For example, you very well might want to treat ' +
+    'Categorical and Continuous data differently! This can be achieved by setting different scopes. ' +
+    'Note that is the iterative imputer is selected, then scope also controls the type (regressor vs. classifier) ' +
+    'of the base model. I.e., regressors will be shown unless the scope is Categorical, then classifier options will be shown.';
     var obj_label = getPopLabel(key, "Imputer ", obj_descr, '-obj-input');
 
     var html = getBaseObjHTML(key, obj_label);
@@ -128,7 +150,13 @@ function getBaseImputerHTML(key) {
 
 function getBaseScalerHTML(key) {
 
-    var obj_descr = 'Selection of Scaler to use';
+    var obj_descr = 'This defines the optional choice of scaler object. ' +
+    'Within BPt the Scaler refers to any piece within the ML Pipeline which are ' +
+    'responsible for the scaling / normalizing functionality. Specifically, ' +
+    'this can be thought of as any transformation upon the data which does not require information about ' +
+    'the target variable, does not change the number of features and acts on each feature independently. ' +
+    'For example, a common scaling procedure is to ' +
+    'employ the StandardScaler option, which sets all features individually to have mean 0 and standard deviation of 1.';
     var obj_label = getPopLabel(key, "Scaler ", obj_descr, '-obj-input');
     var html = getBaseObjHTML(key, obj_label);
 
@@ -140,7 +168,12 @@ function getBaseScalerHTML(key) {
 
 function getBaseTransformerHTML(key) {
 
-    var obj_descr = 'Selection of Transformer to use';
+    var obj_descr = 'This parameter controls the optional selection of a Transformer with the ML Pipeline. ' +
+    'Specifically, the choice of Transformer with the BPt refers to any type of transformation to the loaded data ' +
+    'which may change the number of features in a non-simple way (i.e., conceptually distinct from Feat Selectors). ' +
+    'These are transformations like applying Principle Component Analysis (PCA), or on the fly One Hot Encoding. ' + 
+    'As with other pipeline pieces, scopes can be set to apply these transformation on more specific subsets of the data.';
+
     var obj_label = getPopLabel(key, "Transformer ", obj_descr, '-obj-input');
     var html = getBaseObjHTML(key, obj_label);
 
