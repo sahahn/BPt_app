8f29caa972137a40ca2e443e732afd7ecd921688
sahahn <sahahn@uvm.edu>
2020-09-21 15:01:38 -0400

    chng

--- WebApp/php/setup.php
+++ WebApp/php/setup.php
@@ -1,16 +1,16 @@
 <?php
 include '/var/www/html/data/config.php';
 
-$cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
-$cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup_info.py"';
-$cmd = $cmd_p1.$cmd_p2;
-exec($cmd);
-
 // If existing ready, delete it
 $ready_loc = $data_dr.'ready';
 if (file_exists($ready_loc)) {
     unlink($ready_loc);
 }
+
+$cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
+$cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc.'setup_info.py"';
+$cmd = $cmd_p1.$cmd_p2;
+exec($cmd);
 
 $cmd_p1 = "/bin/bash -c \". /etc/profile.d/conda.sh; conda activate ML; ";
 $cmd_p2 = "/opt/conda/envs/ML/bin/python ".$python_loc."setup_db.py > /dev/null &\"";
