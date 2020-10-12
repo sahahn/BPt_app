e56b733c26d9b2d72dfe0f2d5c2399d6aefba4ba
sahahn <sahahn@uvm.edu>
2020-10-12 13:42:37 -0400

    dfg

--- WebApp/index.php
+++ WebApp/index.php
@@ -80,7 +80,7 @@
   </nav>
 
   <nav class="navbar fixed-top bg-light navbar-light" style="margin-left: 15em; padding: .5em; height:3.65em; border-bottom: 1px solid rgba(0,0,0,.1);">
-    <a id="top-text" class="text-center col-md-6" style="padding-left:0px;"></a>
+    
     <a class="float-right"><button id="delete-project" type="button" class="btn btn-sm btn-danger" data-toggle="modal"
       data-target="#delete-confirm" style="display:none;">Delete Project <i class="fas fa-trash-alt"></i></button></a>
     <a class="float-right"><button id="save-projects" type="button" class="btn btn-sm btn-success">Save Projects 
