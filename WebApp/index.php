57840485f8d04380ac16d71a2f3a60a08e3b0fc8
sahahn <sahahn@uvm.edu>
2020-10-12 13:22:40 -0400

    dfg

--- WebApp/index.php
+++ WebApp/index.php
@@ -79,7 +79,7 @@
     </div>
   </nav>
 
-  <nav class="navbar fixed-top navbar-light" style="height:4em; background-color: #b9c0c8;">
+  <nav class="navbar fixed-top navbar-light" style="margin-left: 15em; height:4em; background-color: #b9c0c8;">
     ww
   </nav>
 
