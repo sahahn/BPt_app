a4ea76449ce22176e053f8bd23cc42ff26ac82bd
sahahn <sahahn@uvm.edu>
2020-10-12 13:25:34 -0400

    try

--- WebApp/index.php
+++ WebApp/index.php
@@ -79,7 +79,7 @@
     </div>
   </nav>
 
-  <nav class="navbar fixed-top navbar-light" style="margin-left: 15em; height:4em; background-color: #b9c0c8;">
+  <nav class="navbar fixed-top navbar-light" style="margin-left: 15em; padding: .5em; height:4em; background-color: #f1f1f1;">
     ww
   </nav>
 
