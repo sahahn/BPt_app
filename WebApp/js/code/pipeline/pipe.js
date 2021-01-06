e5580eaded8409babdfd56a8d591f41d45fc267c
sahahn <sahahn@uvm.edu>
2021-01-06 11:44:31 -0500

    tes

--- WebApp/js/code/pipeline/pipe.js
+++ WebApp/js/code/pipeline/pipe.js
@@ -199,6 +199,9 @@ function getType(project, key) {
 }
 
 function getByTypeOptions(project, obj, key) {
+    
+    console.log(obj)
+    console.log(key)
 
     var type = getType(project, key);
     return ML_options[obj][type];
